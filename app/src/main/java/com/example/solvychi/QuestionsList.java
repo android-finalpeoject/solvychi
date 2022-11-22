5237850e39279da3756f212b338b58e330576ff9
ghizlane kassimi <ghizlanekassimi8@gmail.com>
2022-11-22 16:22:32 +0100

    changes

--- app/src/main/java/com/example/solvychi/QuestionsList.java
+++ app/src/main/java/com/example/solvychi/QuestionsList.java
@@ -2,13 +2,16 @@ package com.example.solvychi;
 
 public class QuestionsList {
     private String texte;
+    private String texte2;
     private int image;
+    private int background;
 
-    public QuestionsList(String texte , int image) {
-        this.texte =texte;
-        this.image=image;
+    public QuestionsList(String texte, String texte2, int image, int background) {
+        this.texte = texte;
+        this.texte2 = texte2;
+        this.image = image;
+        this.background = background;
     }
-
 
 
     public String getTexte() {
@@ -17,5 +20,13 @@ public class QuestionsList {
 
     public int getImage() {
         return image;
+    }
+
+    public String getTexte2() {
+        return texte2;
+    }
+
+    public int getBackground() {
+        return background;
     }
 }
